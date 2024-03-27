3531e506dfc8ad623b71068503b9e78570a6222d
afinn12 <113125490+afinn12@users.noreply.github.com>
2024-03-27 18:01:45 -0400

    Add files via upload

--- ProbabilisticAgent.java
+++ ProbabilisticAgent.java
@@ -2,13 +2,15 @@ package src.pas.battleship.agents;
 
 
 // SYSTEM IMPORTS
-
+import java.util.*;
 
 // JAVA PROJECT IMPORTS
 import edu.bu.battleship.agents.Agent;
 import edu.bu.battleship.game.Game.GameView;
+import edu.bu.battleship.game.EnemyBoard;
 import edu.bu.battleship.game.EnemyBoard.Outcome;
 import edu.bu.battleship.utils.Coordinate;
+import edu.bu.battleship.game.PlayerView;
 
 
 public class ProbabilisticAgent
@@ -24,8 +26,29 @@ public class ProbabilisticAgent
     @Override
     public Coordinate makeMove(final GameView game)
     {
+        Random rand = new Random();
+        //System.out.println(game.getEnemyShipTypeToNumRemaining());
 
-        return null;
+        //find size of board to use
+        int max = 0;
+        while(game.isInBounds(max+1, max+1)){
+            max++;
+        }
+
+
+        Outcome[][] result = game.getEnemyBoardView();
+       
+        //pick random coordinate until not attacked yet
+        int x = rand.nextInt(max);
+        int y = rand.nextInt(max);
+        while(!result[x][y].equals(Outcome.UNKNOWN)){
+            x = rand.nextInt(max);
+            y = rand.nextInt(max);
+        }
+        System.out.println("c:" +x+","+y+"   "+result[x][y]);
+        
+        Coordinate coor = new Coordinate(x,y);
+        return coor;
     }
 
     @Override
